a63fae0bc7b4dbd56d932a3ff98909b3068b764a
Manikandan <nrgmanikandan91@gmail.com>
2026-02-11 19:24:09 +0530

    form_hook.php issue fixed

--- local/ALX_cdn_scorm/classes/form_hook.php
+++ local/ALX_cdn_scorm/classes/form_hook.php
@@ -14,19 +14,47 @@ class form_hook {
     public static function extend_scorm_form($mform) {
         global $CFG;
 
-        // Ensure we are in the SCORM module form
-        if ($mform->elementExists('scormtype')) {
-            $scormtype = $mform->getElement('scormtype');
+        // Check if we are interacting with the SCORM module form
+        $classname = get_class($mform);
+        if (strpos($classname, 'mod_scorm_mod_form') === false) {
+            return;
+        }
+
+        // We need the internal MoodleQuickForm object which is protected as $_form.
+        // We use Reflection to access it properly.
+        $form = $mform;
+        if (method_exists($mform, 'get_form')) {
+             $form = $mform->get_form();
+        } else {
+             try {
+                $ref = new \ReflectionClass($mform);
+                if ($ref->hasProperty('_form')) {
+                    $prop = $ref->getProperty('_form');
+                    $prop->setAccessible(true);
+                    $form = $prop->getValue($mform);
+                }
+             } catch (\Exception $e) {
+                // Should not happen, but fallback
+             }
+        }
+        
+        // Final sanity check: does $form have addElement?
+        if (!is_object($form) || !method_exists($form, 'addElement')) {
+            return;
+        }
+
+        // Now use $form to add elements
+        if ($form->elementExists('scormtype')) {
             
             // Add a header/separator for clarity
-            $mform->addElement('header', 'local_alx_cdn_header', get_string('pluginname', 'local_alx_cdn_scorm'));
+            $form->addElement('header', 'local_alx_cdn_header', get_string('pluginname', 'local_alx_cdn_scorm'));
 
-            $mform->addElement('checkbox', 'local_alx_cdn_enable', get_string('enablecdn', 'local_alx_cdn_scorm'), get_string('enablecdn_desc', 'local_alx_cdn_scorm'));
-            $mform->addHelpButton('local_alx_cdn_enable', 'enablecdn', 'local_alx_cdn_scorm');
+            $form->addElement('checkbox', 'local_alx_cdn_enable', get_string('enablecdn', 'local_alx_cdn_scorm'), get_string('enablecdn_desc', 'local_alx_cdn_scorm'));
+            $form->addHelpButton('local_alx_cdn_enable', 'enablecdn', 'local_alx_cdn_scorm');
             
-            $mform->addElement('text', 'local_alx_cdn_url', get_string('cdnurl', 'local_alx_cdn_scorm'), array('size' => 60));
-            $mform->setType('local_alx_cdn_url', PARAM_RAW); 
-            $mform->hideIf('local_alx_cdn_url', 'local_alx_cdn_enable', 'notchecked');
+            $form->addElement('text', 'local_alx_cdn_url', get_string('cdnurl', 'local_alx_cdn_scorm'), array('size' => 60));
+            $form->setType('local_alx_cdn_url', PARAM_RAW); 
+            $form->hideIf('local_alx_cdn_url', 'local_alx_cdn_enable', 'notchecked');
         }
     }
 }
